2a9ef6d03ae459afcc3c78ee8256734414b3c3d2
sysbird <koro@sysbird.jp>
2022-03-04 12:25:33 +0900

    add vk product icon

--- vksgn-template.php
+++ vksgn-template.php
@@ -1,31 +1,31 @@
 <div class="vksgn-navigation">
-	<ul class="vksgn-main">
-		<li class="vksgn-main-product"><a href="#"><i class="fas fa-glasses"></i>製品を見る</a>
+	<ul class="vksgn-primary">
+		<li class="vksgn-primary-item product"><a href="#"><i class="fas fa-glasses"></i>製品を見る</a>
 			<div class="vksgn-sub">
-				<ul class="vksgn-sub-nav">
-					<li><a href="https://lightning.vektor-inc.co.jp/"><i class="fas fa-bolt"></i>Lightning</a></li>
-					<li><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-blocks/"><i class="fas fa-bolt"></i>VK Blocks</a></li>
-					<li><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-block-patterns/"><i class="fas fa-bolt"></i>VK Block Patterns</a></li>
-					<li><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-filter-search/"><i class="fas fa-bolt"></i>VK Filter Search</a></li>
+				<ul class="vksgn-sub-items">
+					<li class="vksgn-sub-item"><a href="https://lightning.vektor-inc.co.jp/"><i class="fas fa-bolt"></i>Lightning</a></li>
+					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-blocks/"><i class="vk-icon"></i>VK Blocks</a></li>
+					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-block-patterns/"><i class="vk-icon"></i>VK Block Patterns</a></li>
+					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-filter-search/"><i class="vk-icon"></i>VK Filter Search</a></li>
 				</ul>
 				<div class="vksgn-sub-title"><a href="#">製品一覧<i class="fas fa-chevron-right"></i></a></div>
 			</div>
 		</li>
-		<li><a href="https://vws.vektor-inc.co.jp/"><i class="fas fa-shopping-cart"></i>購入する</a></li>
-		<li><a href="https://showcase.vektor-inc.co.jp/"><i class="fas fa-desktop"></i>学習する</a></li>
-		<li class="vksgn-main-join"><a href="#"><i class="fas fa-users"></i>参加する</a>
+		<li class="vksgn-primary-item"><a href="https://vws.vektor-inc.co.jp/"><i class="fas fa-shopping-cart"></i>購入する</a></li>
+		<li class="vksgn-primary-item"><a href="https://showcase.vektor-inc.co.jp/"><i class="fas fa-desktop"></i>学習する</a></li>
+		<li class="vksgn-primary-item join"><a href="#"><i class="fas fa-users"></i>参加する</a>
 			<div class="vksgn-sub">
-				<ul class="vksgn-sub-nav">
-					<li><a href="https://vws.vektor-inc.co.jp/"><i class="fas fa-chalkboard-teacher"></i>VWS<br>
+				<ul class="vksgn-sub-items">
+					<li class="vksgn-sub-item"><a href="https://vws.vektor-inc.co.jp/"><i class="fas fa-chalkboard-teacher"></i>VWS<br>
 					オンライン勉強会</a></li>
-					<li><a href="https://vws.vektor-inc.co.jp/vws-community"><i class="fab fa-slack"></i>VWS<br>
+					<li class="vksgn-sub-item"><a href="https://vws.vektor-inc.co.jp/vws-community"><i class="fab fa-slack"></i>VWS<br>
 					ユーザーSlack</a></li>
-					<li><a href="https://vws.vektor-inc.co.jp/forums"><i class="far fa-comments"></i>VWS<br>
+					<li class="vksgn-sub-item"><a href="https://vws.vektor-inc.co.jp/forums"><i class="far fa-comments"></i>VWS<br>
 					フォーラム</a></li>
 				</ul>
 				<div class="vksgn-sub-title"><a href="#"> オンラインコミュニティ一覧<i class="fas fa-chevron-right"></i></a></div>
 			</div>
 		</li>
-		<li><a href="https://showcase.vektor-inc.co.jp/"><i class="fas fa-handshake"></i>依頼する</a></li>
+		<li class="vksgn-primary-item"><a href="https://showcase.vektor-inc.co.jp/"><i class="fas fa-handshake"></i>依頼する</a></li>
 	</ul>
 </div>
