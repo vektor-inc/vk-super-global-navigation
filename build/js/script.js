ca7afc7d90f5a23a8a1096c243041959a141fe8b
sysbird <koro@sysbird.jp>
2022-03-07 15:42:36 +0900

    update

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,5 @@
 ;(function ($, document, window) {
 	$(document).ready(function ($) {
-			$(this).has('.vksgn-sub').toggleClass("open");
 		$('.vksgn-navigation.is_mobile .vksgn-primary-item').click(function () {
 			$(this).has('.vksgn-sub').toggleClass("open");
 			$(this).siblings().removeClass("open");
