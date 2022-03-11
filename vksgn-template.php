4312a49bfcb391c4ef9515ae333decbc9ee17a94
TORIYAMA Yuko <koro@sysbird.jp>
2022-03-11 17:11:49 +0900

    change link url

--- vksgn-template.php
+++ vksgn-template.php
@@ -8,7 +8,7 @@
 				<ul class="vksgn-sub-items">
 					<li class="vksgn-sub-item"><a href="https://lightning.vektor-inc.co.jp/"><i class="fas fa-bolt"></i>Lightning</a></li>
 					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-blocks/"><i class="vk-icon"></i>VK Blocks</a></li>
-					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-block-patterns/"><i class="vk-icon"></i>VK Block Patterns</a></li>
+					<li class="vksgn-sub-item"><a href="https://patterns.vektor-inc.co.jp/"><i class="vk-icon"></i>VK Pattern Library</a></li>
 					<li class="vksgn-sub-item"><a href="https://www.vektor-inc.co.jp/service/wordpress-plugins/vk-filter-search/"><i class="vk-icon"></i>VK Filter Search</a></li>
 				</ul>
 				<div class="vksgn-sub-title"><a href="#">製品一覧<i class="fas fa-chevron-right"></i></a></div>
@@ -30,6 +30,6 @@
 				<div class="vksgn-sub-title"><a href="#"> オンラインコミュニティ一覧<i class="fas fa-chevron-right"></i></a></div>
 			</div>
 		</li>
-		<li class="vksgn-primary-item"><a href="https://showcase.vektor-inc.co.jp/"><i class="fas fa-handshake"></i>依頼する</a></li>
+		<li class="vksgn-primary-item"><a href="https://showcase.vektor-inc.co.jp/archives/author/wpmaster"><i class="fas fa-handshake"></i>実例を見る</a></li>
 	</ul>
 </div>
